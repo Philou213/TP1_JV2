dda8cd1b4b3e7666ac7c3f64add46c8c747fd80d
Philou213 <philsr2115@gmail.com>
2023-09-29 11:51:57 -0400

    Bullet movement

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
+    }
+}
