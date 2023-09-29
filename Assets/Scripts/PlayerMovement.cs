fa9f238bfc0f026e330ff7c3cb51fc356c7ad6ea
Philou213 <philsr2115@gmail.com>
2023-09-29 11:00:22 -0400

    PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private Vector3 direction;
+    private CharacterController characterController;
+    private float rotationTime = 0.1f;
+    private float rotationSpeed;
+    private float gravity = 30f;
+    private float jumpSpeed = 12f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BuildSurfaceMovement();
+        characterController.Move(direction);
+    }
+
+    private void BuildSurfaceMovement()
+    {
+        //TODO:Get axis raw for gamepad
+        //Get inputs
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        direction = new Vector3(horizontal, 0f, vertical);
+
+        //Cancel higher movement speed
+        if (direction.magnitude > 1.0f) direction = direction.normalized;
+
+        // Move only if not in deadzone
+        if (direction.magnitude >= 0.1f)
+        {
+            //Get angle of movement
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            //Smooth angle
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, rotationTime);
+
+            //Set player rotation
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //Set direction vector
+            direction.x = direction.x * movementSpeed * Time.deltaTime;
+            direction.z = direction.z * movementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            //No movement
+            direction = Vector3.zero;
+        }
+    }
+}
